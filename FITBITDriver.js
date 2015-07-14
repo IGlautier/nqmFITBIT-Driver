1b5057d9192003395086b5fe2955e61d9f166c43
IGlautier <ivanglautier@gmail.com>
2015-07-14 11:51:06 +0100

    Working Driver

--- FITBITDriver.js
+++ FITBITDriver.js
@@ -1,7 +1,3 @@
-"use strict";
-
-module.exports = (function() {
-
 	var util = require("util");
 	var EventEmitter = require("events").EventEmitter;
 	var passport = require("passport");
@@ -12,7 +8,9 @@ module.exports = (function() {
 	var Getter = require("./FITBITGetter.js").Getter;
 	var process = require("./FITBITProcess.js");
 	var configLocal = require("./config.local.json"), fs = require("fs"); 
-	
+	var tConf = require("./config.json");
+	var bodyParser = require('body-parser');
+	var multer = require('multer'); 
 
 
 	function FITBITDriver(config) {
@@ -44,7 +42,17 @@ module.exports = (function() {
 		this._server.use(passport.initialize());
 		this._server.use(passport.session());
 		this._server.use(cookieParser());
+		this._server.use(bodyParser.json()); // for parsing application/json
+		this._server.use(bodyParser.urlencoded({ extended: true })); // for parsing application/x-www-form-urlencoded
+		this._server.use(multer()); // for parsing multipart/form-data
 		
+		passport.serializeUser(function(user, done) {
+		  done(null, user);
+		});
+
+		passport.deserializeUser(function(user, done) {
+		  done(null, user);
+		});
 		/*OAuth for getting access token and subscribing user*/
 		passport.use(new FitbitStrategy({
 			consumerKey: this._config.appData.key,
@@ -52,6 +60,8 @@ module.exports = (function() {
 			callbackURL: this._config.callbackURL
 		},
 			function(token, tokenSecret, profile, done) {
+				console.log(token);
+				console.log(tokenSecret);
 				var accessToken = {
 					public: token,
 					secret: tokenSecret
@@ -77,6 +87,7 @@ module.exports = (function() {
 						
 					});
 				});
+				return done(null, profile);
 			}.bind(this)
 		));
 		
@@ -90,6 +101,7 @@ module.exports = (function() {
 			// Currently makes the user's page hang
 			res.redirect('/');
 		});
+		
 		  
 		/* Failure page*/ 
 		this._server.get('/auth/failed', function(req,res) {
@@ -102,25 +114,34 @@ module.exports = (function() {
 		this._server.post('/subscribe/notify', function(req,res) {
 
 			res.send(204);
-			for(var i = 0; i < req.body.length; i++) {
-				var uID = req.body[i].ownerID;
-				updater.get(configLocal.users.uID.accessToken, function(response) {
+			
+			if(typeof req.body.errors === "undefined"){
 				
-					if(response.error) console.log("Could not get update for user ID " + uID);
+				for(var i = 0; i < req.body.length; i++) {
 					
-					else {
-						process.parseData(response.data, function(parseResult) {
-						
-							if (parseResult.error) console.log("Failed to parse update for user ID " + uID);
-							
-							else this.emit("data", "FITBIT"+uID, parseResult.userData);
-						});
-					}
-				});	
-				
-			}
+					var uID = req.body[i].ownerId;
+					console.log("Fetching data for user id: " + uID);
+					
+					this._updater.get(configLocal.users[i].access, function(response) {
 
-		});
+						if(response.error) console.log("Could not get update for user");
+						
+						else {
+							process.parseData(response.data, function(parseResult) {
+							
+								if (parseResult.error) console.log("Failed to parse update for user");
+								
+								else console.log(util.inspect(parseResult.userData));
+								//this.emit("data", "FITBIT"+uID, parseResult.userData);
+							});
+						}
+					});	
+					
+				}
+			}
+			else console.log(req.body.errors);
+
+		}.bind(this));
 		
 		console.log(this._config.port)
 
@@ -131,6 +152,6 @@ module.exports = (function() {
 	  FITBITDriver.prototype.stop = function() {
 		this._server.close();
 	  };
-
-	  return FITBITDriver;
-}());
+	
+var server = new FITBITDriver(tConf);
+server.start();
--- FITBITProcess.js
+++ FITBITProcess.js
@@ -3,11 +3,11 @@ var exports = module.exports = {};
 exports.parseData = function(data, callback) {
 
 	var parseResult = {};
-	
-	if (data === "undefined") parseResult.error = true;
+
+	if (typeof data.errors != "undefined") parseResult.error = true;
 	
 	else {
-	
+		console.log(data);
 		parseResult.userData = {};
 		parseResult.error = false;
 		parseResult.userID = data.user.encodedID;
@@ -23,8 +23,12 @@ exports.parseData = function(data, callback) {
 exports.checkUser = function(user, users, callback) {
 
 	var exists = false;
+	console.log(user);
 	
-	for (var i = 0; i < users.length; i++) if (users[i].ID == user) exists = true;
+	for (var i = 0; i < users.length; i++) {
+		console.log(users[i].ID);
+		if (users[i].ID === user) exists = true;
+	}
 	
 	callback(exists, user);
 }
